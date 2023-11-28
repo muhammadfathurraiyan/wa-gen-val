27f36957a761815cef3a6e6a48ae15b32c689964
muhammadfathurraiyan <raiyanfathur@gmail.com>
2023-11-28 19:45:51 +0700

    deploy test11

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-var webpack = require("webpack");
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["puppeteer"],
@@ -7,46 +6,10 @@ const nextConfig = {
 };
 
 module.exports = {
-  target: "node",
-  output: {
-    libraryTarget: "commonjs2",
-  },
-  module: {
-    rules: [
-      {
-        // For node binary relocations, include ".node" files as well here
-        test: /\.(m?js|node)$/,
-        // it is recommended for Node builds to turn off AMD support
-        parser: { amd: false },
-        use: {
-          loader: "@vercel/webpack-asset-relocator-loader",
-          options: {
-            // optional, base folder for asset emission (eg assets/name.ext)
-            outputAssetBase: "assets",
-            // optional, restrict asset emissions to only the given folder.
-            filterAssetBase: process.cwd(),
-            // optional, permit entire __dirname emission
-            // eg `const nonAnalyzable = __dirname` can emit everything in the folder
-            emitDirnameAll: false,
-            // optional, permit entire filterAssetBase emission
-            // eg `const nonAnalyzable = process.cwd()` can emit everything in the cwd()
-            emitFilterAssetBaseAll: false,
-            // optional, custom functional asset emitter
-            // takes an asset path and returns the replacement
-            // or returns false to skip emission
-            customEmit: (path, { id, isRequire }) =>
-              false | '"./custom-replacement"',
-            // optional, a list of asset names already emitted or
-            // defined that should not be emitted
-            existingAssetNames: [],
-            wrapperCompatibility: false, // optional, default
-            // build for process.env.NODE_ENV = 'production'
-            production: true, // optional, default is undefined
-            cwd: process.cwd(), // optional, default
-            debugLog: false, // optional, default
-          },
-        },
-      },
-    ],
-  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": "development",
+      "process.env.FLUENTFFMPEG_COV": false,
+    }),
+  ],
 };
