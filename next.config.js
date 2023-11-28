af82716c523d3433465c69467c2bf825795728cd
muhammadfathurraiyan <raiyanfathur@gmail.com>
2023-11-28 18:54:16 +0700

    deploy test4

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["puppeteer"],
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.FLUENTFFMPEG_COV": false,
+    }),
+  ],
 };
 
 module.exports = nextConfig;
