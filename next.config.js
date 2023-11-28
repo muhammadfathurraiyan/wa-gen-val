cba255c5dc1bd3fd8940edab62e1b7fde9748901
muhammadfathurraiyan <raiyanfathur@gmail.com>
2023-11-28 19:01:42 +0700

    deploy test5

--- next.config.js
+++ next.config.js
@@ -6,21 +6,9 @@ const nextConfig = {
 };
 
 module.exports = {
-  // Your other Next.js configuration options go here
-
-  // Add or modify plugins configuration
-  webpack: (config, { isServer }) => {
-    // Modify the Webpack configuration for both client and server builds
-
-    // Example: Adding a DefinePlugin to set FLUENTFFMPEG_COV to false
-    config.plugins.push(
-      new this.webpack.DefinePlugin({
-        "process.env.FLUENTFFMPEG_COV": false,
-      })
-    );
-
-    // Additional plugin configurations or modifications can go here
-
-    return config;
-  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.FLUENTFFMPEG_COV": false,
+    }),
+  ],
 };
